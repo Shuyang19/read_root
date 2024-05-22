9b735b7ae1080675e14a196f88550af548e94072
Shuyang19 <shz061@ucsd.edu>
2024-05-22 12:53:58 -0700

    test

--- read.C
+++ read.C
@@ -70,14 +70,15 @@ std::cout << "check point 7" << std::endl;
     Long64_t nEntries_300 = tree_300->GetEntries();
 std::cout <<"assign bins" << std::endl;   
  float w = 1.0;//weight
- std::cout << "initialize weight" << std::cout;
+ std::cout << "initialize weight" << std::endl;
  int count = 0;
     for (Long64_t i = 0; i < nEntries_300; i++) {
         count++;
-        std::cout <<"entering the for loop: " << count << std::endl;
+        //std::cout <<"entering the for loop: " << count << std::endl;
         tree_300->GetEntry(i);
-        std::cout << "get entries" << std::endl;
+        //std::cout << "get entries" << std::endl;
         h_gLLP_ctau_300->Fill(gLLP_ctau_300, w);
+        std::cout << "fill the graph"<< std::endl;
     }
 std::cout << "fill the graph" << std::endl;    
 h_gLLP_ctau_300->Scale(1./h_gLLP_ctau_300->Integral(0,-1));
